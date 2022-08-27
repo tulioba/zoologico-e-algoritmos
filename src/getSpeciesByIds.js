ec1398b51729eca98a05c0a159c2628b386bfde0
tulio <tulio.amorimm@gmail.com>
2022-08-27 01:10:10 -0300

    resolve lint requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -2,7 +2,7 @@ const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getSpeciesByIds(...ids) {
-  return species.filter((specie) => (ids.includes(specie.id) ? true : false));
+  return species.filter((specie) => ids.includes(specie.id));
 }
 
 console.log(getSpeciesByIds());
