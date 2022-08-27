8f18137da22ebddc3d3630534d453bb1528e1385
tulio <tulio.amorimm@gmail.com>
2022-08-27 01:04:21 -0300

    resolve lint do requisito 1

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,11 +1,7 @@
-const { species } = require('../data/zoo_data');
-const data = require('../data/zoo_data');
+const { species } = require("../data/zoo_data");
+const data = require("../data/zoo_data");
 
-function getAnimalsOlderThan(animal, age) {
-  
-}
-console.log(getAnimalsOlderThan())
+function getAnimalsOlderThan(animal, age) {}
+console.log(getAnimalsOlderThan());
 
 module.exports = getAnimalsOlderThan;
-
-
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,9 @@
-const { species } = require("../data/zoo_data");
-const data = require("../data/zoo_data");
+const { species } = require('../data/zoo_data');
+const data = require('../data/zoo_data');
 
-function getSpeciesByIds(...ids) {return species.filter((specie) => ids.includes(specie.id) ? true : false)}
+function getSpeciesByIds(...ids) {
+  return species.filter((specie) => (ids.includes(specie.id) ? true : false));
+}
 
 console.log(getSpeciesByIds());
 
