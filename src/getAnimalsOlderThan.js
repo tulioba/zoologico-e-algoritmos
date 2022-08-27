bcd59c2517bdf0944c22c208bc9cf62946cf616a
tulio <tulio.amorimm@gmail.com>
2022-08-27 00:59:34 -0300

    adiciona requisito 1

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,11 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  
 }
+console.log(getAnimalsOlderThan())
 
 module.exports = getAnimalsOlderThan;
+
+
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,8 @@
-const data = require('../data/zoo_data');
+const { species } = require("../data/zoo_data");
+const data = require("../data/zoo_data");
 
-function getSpeciesByIds(ids) {
-  
-}
+function getSpeciesByIds(...ids) {return species.filter((specie) => ids.includes(specie.id) ? true : false)}
+
+console.log(getSpeciesByIds());
 
 module.exports = getSpeciesByIds;
