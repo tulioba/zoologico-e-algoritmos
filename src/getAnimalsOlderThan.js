23377d57d215cefb83dadb05d5c0cd3e85e637fb
tulio <tulio.amorimm@gmail.com>
2022-08-29 12:50:54 -0300

    resolve lint requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,22 +1,17 @@
-const { species } = require("../data/zoo_data");
-const data = require("../data/zoo_data");
+const { species } = require('../data/zoo_data');
+const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  const animalObject = species.find((specie) => {
-    return specie.name === animal;
-  });
-  const ageObject = animalObject.residents.filter((element) => {
-    return element.age >= age;
-  });
+  const animalObject = species.find((specie) => specie.name === animal);
+  const ageObject = animalObject.residents.filter((element) => element.age >= age);
   if (ageObject.length === animalObject.residents.length) {
     return true;
-  } else {
-    return false;
   }
+  return false;
 }
 
 // console.log(getAnimalsOlderThan("lions", 5));
-console.log(getAnimalsOlderThan("otters", 20));
+console.log(getAnimalsOlderThan('otters', 20));
 module.exports = getAnimalsOlderThan;
 
 // /O que será testado:
