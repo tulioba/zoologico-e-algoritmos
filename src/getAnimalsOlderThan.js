2fd01c48826ba4531677d9a23f22cc5d683e8eca
tulio <tulio.amorimm@gmail.com>
2022-08-30 14:55:52 -0300

    lint

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -2,7 +2,7 @@ const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  const animalObject = species.find((specie) => specie.name = animal);
+  const animalObject = species.find((specie) => specie.name === animal);
   const ageObject = animalObject.residents.filter((element) => element.age >= age);
   if (ageObject.length === animalObject.residents.length) {
     return true;
