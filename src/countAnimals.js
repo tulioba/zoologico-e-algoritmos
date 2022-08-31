0dcb459afe823568547c2985d6a1718d3eed618b
tulio <tulio.amorimm@gmail.com>
2022-08-30 22:01:11 -0300

    adiciona requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,42 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
-}
+  if (!animal) {
+    return species.reduce((acc, curr) => {
+    acc[curr.name] = curr.residents.length
+    return acc
+    }, {})
+  }
 
+if (animal) {
+  const specieAnimal = species.filter((specie) =>  specie.name === animal.specie);
+  const animalAmount = specieAnimal.map((specie) => specie.residents).flat(); 
+  return animalAmount.reduce((acc, curr) => {
+    if(curr.sex === animal.sex || !animal.sex) {
+      acc += 1
+    }
+    return acc
+  }, 0)
+  }
+  
+//   if (animal.specie) {
+//     const specieAnimal = species.filter((specie) =>  specie.name === animal.specie);
+//     const animalAmount2 = specieAnimal.map((specie) => specie.residents).flat();
+//     return animalAmount2.length
+    
+//   }
+// const specieAnimal = species.find((specie) => specie.name === animal.specie);
+// if(animal.sex) {
+//   return specieAnimal.residents.filter((resident) => resident.sex === animal.sex).length
+// } 
+// else {
+//   return specieAnimal.residents.length
+// }
+
+}
+  console.log(countAnimals({ specie: 'giraffes', sex: 'female' }))
+  
 module.exports = countAnimals;
+
+
--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,21 @@
+const { species } = require('../data/zoo_data');
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+    text('testando se retorna a quantidade de elefantes', () => {
+        expect(const ele = species.filter((specie) => {
+            return specie.name === 'elephants'
+        }))
+    })
+});
+
+
+
+
+
+
+
+
+// test('Teste se productDetails é uma função.', () => {
+//     expect(typeof productDetails).toEqual('function');
+//   });
