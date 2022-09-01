e5d2afd6dad296fe080f1a0a64a7ea885543f920
tulio <tulio.amorimm@gmail.com>
2022-09-01 13:25:34 -0300

    adiciona requistos 8 e 12

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -3,34 +3,29 @@ const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
   const visiters = { child: 0, adult: 0, senior: 0 };
-  const age = entrants.map((entrant) => entrant.age);
-  console.log(age);
-  for (const index of age) {
-    if (index < 18) {
-      visiters.child += 1;
-    } else if (index >= 18 && index < 50) {
-      visiters.adult += 1;
-    } else if (index >= 50) {
-      visiters.senior += 1;
+  const ages = entrants.map((entrant) => entrant.age);
+  return ages.reduce((acc, curr) => {
+    if (curr < 18) {
+      acc.child += 1;
+    } else if (curr < 50) {
+      acc.adult += 1;
+    } else {
+      acc.senior += 1;
     }
-  }
-  return visiters;
+    return acc;
+  }, visiters);
 }
 
 function calculateEntry(entrants) {
   if (!entrants) return 0;
+
   if (Object.keys(entrants).length === 0) return 0;
-  const value = entrants.map((entrant) => entrant.age);
-  return parseFloat(value.reduce((acc, curr) => {
-    if (curr < 18) {
-      acc += prices.child;
-    } else if (curr >= 18 && curr < 50) {
-      acc += prices.adult;
-    } else if (curr >= 50) {
-      acc += prices.senior;
-    }
-    return acc;
-  }, 0).toFixed(2));
+
+  const entrantCount = countEntrants(entrants);
+
+  return entrantCount.child * prices.child
+   + entrantCount.adult * prices.adult
+   + entrantCount.senior * prices.senior;
 }
 
 module.exports = { calculateEntry, countEntrants };
--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -1,3 +1,27 @@
 const getOpeningHours = require('../src/getOpeningHours');
 
-describe('Testes da função getOpeningHours', () => {});
+describe('Testes da função getOpeningHours', () => {
+  test('testa se getOpeningHours é uma função', () => {
+    expect(typeof getOpeningHours).toBe('function');
+  });
+  test('testa se getOpeningHours com !param retorna um objeto', () => {
+    expect(getOpeningHours()).toEqual({
+      Tuesday: { open: 8, close: 6 },
+      Wednesday: { open: 8, close: 6 },
+      Thursday: { open: 10, close: 8 },
+      Friday: { open: 10, close: 8 },
+      Saturday: { open: 8, close: 10 },
+      Sunday: { open: 8, close: 8 },
+      Monday: { open: 0, close: 0 },
+    });
+  });
+  test('testa se para os argumentos Tuesday e 09:00-AM retorna The zoo is open', () => {
+    expect(getOpeningHours('Tuesday', '09:00-AM')).toBe('The zoo is open');
+  });
+  test('testa se para os argumentos Wednesday e 09:00-PM retorna The zoo is closed', () => {
+    expect(getOpeningHours('Wednesday', '09:00-PM')).toBe('The zoo is closed');
+  });
+  test('testa se para os argumentos Monday e 09:00-AM retorna The zoo is closed', () => {
+    expect(getOpeningHours('Monday', '09:00-AM')).toBe('The zoo is closed');
+  });
+});
