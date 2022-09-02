2ac5909b88a3995162997b552bcd0dc05c3be2ad
tulio <tulio.amorimm@gmail.com>
2022-09-02 14:44:23 -0300

    adiciona requisito 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,19 @@
+const { employees, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const employeeID = employees.find((specie) => specie.id === id);
+  const animalID = species.find((specie) => employeeID.responsibleFor.includes(specie.id));
+  const animalsInfo = animalID.residents;
+  const olderAnimal = animalsInfo.reduce((acc, curr) => {
+    if (acc.age > curr.age) {
+      return acc;
+    }
+    return curr;
+  });
+  return [olderAnimal.name, olderAnimal.sex, olderAnimal.age];
 }
-
+console.log(getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
 module.exports = getOldestFromFirstSpecies;
+
+// ["Maxwell", "male", 15]
