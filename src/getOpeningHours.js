33f72ea181aecf004fb8b9fd0b5af3999b19d353
tulio <tulio.amorimm@gmail.com>
2022-09-02 01:52:22 -0300

    adiciona requisito 13

--- src/getOpeningHours.js
+++ src/getOpeningHours.js
@@ -63,5 +63,5 @@ const getOpeningHours = (day, dataHour) => {
   message += openOrClosed(period, hour, open, close) ? 'open' : 'closed';
   return message;
 };
-console.log(getOpeningHours('13:2'));
+
 module.exports = getOpeningHours;
--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -24,4 +24,16 @@ describe('Testes da função getOpeningHours', () => {
   test('testa se para os argumentos Monday e 09:00-AM retorna The zoo is closed', () => {
     expect(getOpeningHours('Monday', '09:00-AM')).toBe('The zoo is closed');
   });
+  test('testa se abreviação do dia da semana volta mensagem de error', () => {
+    expect(() => (getOpeningHours('Thu', '09:00-AM'))).toThrow('The day must be valid. Example: Monday');
+  });
+  test('testa se alterada o padrão do formato do horário volta mensagem de error', () => {
+    expect(() => (getOpeningHours('Friday', '09:00-ZM'))).toThrow('The abbreviation must be \'AM\' or \'PM\'');
+  });
+  test('testa se inserido algum diferente de number no locao de horas volta mensagem de error', () => {
+    expect(() => (getOpeningHours('Friday', '0C:00-ZM'))).toThrow('The hour should represent a number');
+  });
+  test('testa se inserido algum diferente de number no locao de minutos volta mensagem de error', () => {
+    expect(() => (getOpeningHours('Friday', '02:P0-ZM'))).toThrow('The minutes should represent a number');
+  });
 });
